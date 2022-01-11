47546462ad7c25d5de3f639b9290db5d5b4edd40
gromero02 <guillermo.romero.n@gmail.com>
2022-01-11 10:13:27 +0100

    Primer Commit de la app

---
+++ app/src/main/java/com/example/act2_ut2_guillermorn/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.act2_ut2_guillermorn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
